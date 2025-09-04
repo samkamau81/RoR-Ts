8b65bf18a5c7d85e900cf883212c39d84e6f6440
samkamau81 <samuelgatoto81@gmail.com>
2025-09-04 21:40:16 +0300

    4th sep updates 2025

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,30 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-console.log("Hello");
-let age = 30;
+let sales = 123_456_789;
+let course = "TypeScript";
+let is_published = true;
+let level;
+function render(document) {
+    console.log(document);
+}
+let numbers = [1, 2, 3];
+numbers.forEach(n => n.toExponential);
+let user = [1, "Mosh"];
+var Size;
+(function (Size) {
+    Size[Size["Small"] = 1] = "Small";
+    Size[Size["Medium"] = 2] = "Medium";
+    Size[Size["Large"] = 3] = "Large";
+})(Size || (Size = {}));
+let mySize = Size.Medium;
+console.log(mySize);
+function calculateTax(income, taxYear = 2022) {
+    if (taxYear < 2022)
+        return income * 1.2;
+    return income * 1.3;
+}
+let employee = {
+    id: 1,
+    name: "Mosh"
+};
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,26 @@
-console.log("Hello");
-let age: number = 30;
+let sales: number = 123_456_789;
+let course: string = "TypeScript";
+let is_published: boolean = true;
+let level;      
 
+function render(document: any) {
+    console.log(document);
+}
+
+let numbers: number[] = [1, 2, 3];
+numbers.forEach(n => n.toExponential);
+
+let user: [number, string] = [1, "Mosh"];
+
+const enum Size { Small = 1, Medium = 2, Large = 3 }
+let mySize: Size = Size.Medium;
+console.log(mySize);
+
+function calculateTax(income: number, taxYear = 2022): number {
+    if (taxYear < 2022)
+        return income * 1.2;
+    return income * 1.3;
+}
+let employee = { 
+    id: 1, 
+    name: "Mosh" };
